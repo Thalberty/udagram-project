177e9702192025a92db98aa8f785fd6c1a935452
Thalbert <thalbert.miranda@gmail.com>
2020-11-28 06:46:34 -0300

    Update port

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environmentUser = {
 export const environmentFeed = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8081/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
